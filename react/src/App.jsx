5ad82b8d22656eb0ec479855ff683f107e687906
GustavAlbert <gustav.gitarrsolo@gmail.com>
2023-04-13 14:48:35 +0200

    First attempt and draft of converting old HTML to React

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { Component, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Filter from './component/filter';
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <Filter />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

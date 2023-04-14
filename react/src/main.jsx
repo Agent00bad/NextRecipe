58deda7ea25a88c11b1946ccd62d05ea91ca9fc3
Jonathan Perlow <jonathan.perlow@iths.se>
2023-04-14 20:49:09 +0200

    Working on jsx early stage

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Recipe from './component/recipe/recipe';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Recipe />
   </React.StrictMode>,
 )

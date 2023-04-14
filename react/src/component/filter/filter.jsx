93426b8edaf92b300f2ac60660e7c0c6a358b93d
GustavAlbert <gustav.gitarrsolo@gmail.com>
2023-04-14 16:07:39 +0200

    Converted HTML to React, first draft functioning. Remvoed App.js, App.css, and asset parent folder. Made Filter to the main and only page/project in this branch (jsx-filter) #21

--- react/src/component/filter/filter.jsx
+++ react/src/component/filter/filter.jsx
@@ -1,75 +1,77 @@
 import React, { Fragment } from "react";
 
-function Filter() {
+import '../../CSS/colorscheme.css';
+import '../../CSS/filter.css';
+import '../../CSS/button.css';
+
+export default function Filter() {
     return(
-        <Fragment>
         <div>
+          <div>
             <div className="flex-container">
-            <h2>Meal Options</h2>
-            <img className="arrow" src="Images/arrow-down.png" alt="Arrow down" />
+              <h2>Meal Options</h2>
+              <img className="arrow" src="Images/arrow-down.png" alt="Arrow down" />
             </div>
             <hr />
             <div className="flex-container">
-            <button>Breakfast</button>
-            <button>Lunch</button>
-            <button>Dinner</button>
-            <button>Match meal with time?</button>
+              <button>Breakfast</button>
+              <button>Lunch</button>
+              <button>Dinner</button>
+              <button>Match meal with time?</button>
             </div>
-        </div>
-        <div>
+          </div>
+          <div>
             <hr />
             <div className="flex-container">
-            <h2>Allergies/Diet</h2>
-            <img className="arrow" src="Images/arrow-down.png" alt="Arrow down" />
+              <h2>Allergies/Diet</h2>
+              <img className="arrow" src="Images/arrow-down.png" alt="Arrow down" />
             </div>
             <hr />
             <div className="flex-container test">
-            <button>Gluten</button>
-            <button className="selected">Nuts</button>
-            <button>Milk</button>
-            <button>Lactose</button>
-            <button className="selected">Vegeterian</button>
-            <button>Vegan</button>
+              <button>Gluten</button>
+              <button className="selected">Nuts</button>
+              <button>Milk</button>
+              <button>Lactose</button>
+              <button className="selected">Vegeterian</button>
+              <button>Vegan</button>
             </div>
-        </div>
-        <div>
+          </div>
+          <div>
             <hr />
             <div className="flex-container test">
-            <h2>Drinks</h2>
-            <img className="arrow" src="Images/arrow-down.png" alt="Arrow down" />
+              <h2>Drinks</h2>
+              <img className="arrow" src="Images/arrow-down.png" alt="Arrow down" />
             </div>
             <hr />
             <div className="flex-container test2">
-            <button>Beer</button>
-            <button>Wine</button>
-            <button>Milk</button>
-            <button>Juice</button>
-            <button>Coffe</button>
-            <button>Tea</button>
+              <button>Beer</button>
+              <button>Wine</button>
+              <button>Milk</button>
+              <button>Juice</button>
+              <button>Coffe</button>
+              <button>Tea</button>
             </div>
-        </div>
-        <div>
+          </div>
+          <div>
             <hr />
             <div className="flex-container">
-            <h2>Match drinks with food</h2>
-            <img className="arrow" src="Images/arrow-down.png" alt="Arrow down" />
+              <h2>Match drinks with food</h2>
+              <img className="arrow" src="Images/arrow-down.png" alt="Arrow down" />
             </div>
             <hr />
             <div className="flex-container test3">
-            <button>Breakfast</button>
-            <button>Lunch</button>
-            <button>Dinner</button>
-            <button>Match meal with time?</button>
+              <button>Breakfast</button>
+              <button>Lunch</button>
+              <button>Dinner</button>
+              <button>Match meal with time?</button>
             </div>
-        </div>
-        <div>
+          </div>
+          <div>
             <div className="flex-container apply-clear">
-            <button id="apply-button">Apply</button>
-            <button id="clear-button">Clear</button>
+              <button id="apply-button">Apply</button>
+              <button id="clear-button">Clear</button>
             </div>
-        </div>
-    </Fragment>
+          </div>
+      </div>
     )
 }
-
-export default Menu;
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
 import './index.css'
+import Filter from './component/filter/filter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Filter />
   </React.StrictMode>,
 )

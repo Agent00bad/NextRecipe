fa5d3168afaf27d490ea9ee9f0f797f6d0a84d71
Imir <imir.norlin96@hotmail.com>
2023-04-16 14:33:10 +0200

    recipe to jsx #23 Uppdaterade integration main

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import Recipe from './component/recipe/recipe';
+import Filter from './component/filter/filter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Recipe />
+    {/* <Recipe /> */}
+    <Filter />
   </React.StrictMode>,
 )

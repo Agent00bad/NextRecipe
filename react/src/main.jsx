1019edef8f0c683bd5d440c425e2af5e14224ce0
Nugget1368 <rneg1@hotmail.com>
2023-04-17 14:35:57 +0200

    Konverterade LoadingPage till react

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Recipe from './component/recipe/recipe';
 import Filter from './component/filter/filter'
 import Index from './component/index';
+import Loading from './component/loading/loading';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Index/>

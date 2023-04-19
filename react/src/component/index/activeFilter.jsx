503bbf42579d1f296db9b5b26ea36a14fa0d9289
Nugget1368 <rneg1@hotmail.com>
2023-04-19 17:15:26 +0200

    Positionerat Filter-boxen efter en fixed position, inte riktigt klar än, krockar med övriga Cards när skärmen minskas

--- react/src/component/index/activeFilter.jsx
+++ react/src/component/index/activeFilter.jsx
@@ -1,18 +1,22 @@
+/********************************
+  Tog ur den här
+  <div className="filter-flex">
+  </div>
+*********************************/
+
 export function ActiveFilter({ filters = [], header }) {
   return (
     <>
-      <div className="filter-flex">
-        <div className="card">
+        <div id="filterCard" className="card">
           <a id="filtersTag" className="filter-tag" href="filter.html">
             <b>Filters</b>
           </a>
           <div className="filter-list">
-            <ul className="activeFilters">
+            <ul id="filterList">
                 {filters.map(filter => (
                     <li><b>{filter}</b></li>
-                ))};
+                ))}
             </ul>
-          </div>
         </div>
       </div>
     </>
--- react/src/component/index/index.jsx
+++ react/src/component/index/index.jsx
@@ -1,42 +1,52 @@
-import "../../CSS/index.css"
-import {Card} from "./card"
-import { ActiveFilter } from "./activeFilter"
-import logo from '../../Images/logo.png';
-import "../../CSS/HeaderFooter.css"
-import menuIcon from '../../Images/menu-icon.png';
-import perfectpair_transparent from '../../Images/perfectpair_transparent.png'
-import userIcon from '../../Images/user-icon.png'
-export default function Index()
-{
-    return(
-        <>
-        <div className="logo">
-    <img src={logo} alt="WelcomeImage" />
-  </div>
-    {/* <!--Displays active filters--> */}
-      <ActiveFilter
-        filters={["Dinner", "Lactose Free", "Gluten Free", "Alcphol Free", "High Protein"]}
-        header={"Active Filter"}
-      />
-  <main>
-      {/* <!--Shows overview recipe details and works as a selection--> */}
-      <div className="recipe-drink-flex">
-        <Card 
-        title="Recipie" 
-        description="Bla Bla Bla"
-        calories="5000"
-        type="dinner"
-        image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fheatherlikesfood.com%2Fwp-content%2Fuploads%2F2015%2F05%2FDSC03691_1.jpg&f=1&nofb=1&ipt=00921f274e58215cdaaa2ef2c7edc54c6403716a6e800ef332ed86d16be48ad5&ipo=images"/>
-        {/*<!--Shows overview drink details and works as a selection-->*/}
-        <Card
-        title="Drink"
-        description="Best drink ever!"
-        calories="1000"
-        type="dinner"
-        image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.VVKcNyqJMGbMrW-u2lKStAHaEo%26pid%3DApi&f=1&ipt=40124abb2e8e4f5e0bac3b38ef1e02a42fd14524aeeab3b88ae5a23d3a50f9d1&ipo=images"
-        />
+import "../../CSS/index.css";
+import { Card } from "./card";
+import { ActiveFilter } from "./activeFilter";
+import logo from "../../Images/logo.png";
+import "../../CSS/HeaderFooter.css";
+import menuIcon from "../../Images/menu-icon.png";
+import perfectpair_transparent from "../../Images/perfectpair_transparent.png";
+import userIcon from "../../Images/user-icon.png";
+export default function Index() {
+  return (
+    <>
+      <div className="logo">
+        <img src={logo} alt="WelcomeImage" />
       </div>
-    </main>
-        </>
-    )
+      <main>
+        {/* <!--Shows overview recipe details and works as a selection--> */}
+       {/* <div className="recipe-drink-flex"> */}
+          {/* <!--Displays active filters--> */}
+          <ActiveFilter
+            filters={[
+              "Dinner",
+              "Lactose Free",
+              "Gluten Free",
+              "Alcohol Free",
+              "High Protein",
+            ]}
+            header={"Active Filter"}
+          />
+          <div className="CardSpace">  {/*Ifall vi vill flytta alla kort*/}
+
+          <Card
+            title="Recipie"
+            description="Bla Bla Bla"
+            calories="5000"
+            type="dinner"
+            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fheatherlikesfood.com%2Fwp-content%2Fuploads%2F2015%2F05%2FDSC03691_1.jpg&f=1&nofb=1&ipt=00921f274e58215cdaaa2ef2c7edc54c6403716a6e800ef332ed86d16be48ad5&ipo=images"
+          />
+          {/*<!--Shows overview drink details and works as a selection-->*/}
+          <Card
+            title="Drink"
+            description="Best drink ever!"
+            calories="1000"
+            type="dinner"
+            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.VVKcNyqJMGbMrW-u2lKStAHaEo%26pid%3DApi&f=1&ipt=40124abb2e8e4f5e0bac3b38ef1e02a42fd14524aeeab3b88ae5a23d3a50f9d1&ipo=images"
+          />
+        {/* </div> */}
+        </div>
+
+      </main>
+    </>
+  );
 }

186ec330882509160c7629d6bdba95cbbdbe48ec
Daniel Aldén .NET <Daniel Aldn .NET@DESKTOP-0QL2OQM>
2023-04-20 11:16:35 +0200

    me and Robert converted header and footer to jsx react format #22

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Header from "./component/header/header"
+import Footer from "./component/footer/footer"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Footer/>
   </React.StrictMode>,
 )

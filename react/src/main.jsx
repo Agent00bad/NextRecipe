cdb339bc9694b1476c0286036031caf169ce3976
Daniel Aldén .NET <Daniel Aldn .NET@DESKTOP-0QL2OQM>
2023-04-20 14:58:20 +0200

    fixed strange error

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-// import App from './App'
-// import './index.css'
-=======
 import Recipe from './component/recipe/recipeMain';
->>>>>>> development
 import Filter from './component/filter/filter'
 import Index from './component/index';
 import Loading from './component/loading/loading';

e859c7bdfc46950923bffca5aeab582713010af5
Daniel Aldén .NET <Daniel Aldn .NET@DESKTOP-0QL2OQM>
2023-04-27 12:20:41 +0200

    fixed minor mistake

--- react/src/JS/fetchApiData.js
+++ react/src/JS/fetchApiData.js
@@ -24,8 +24,7 @@ export async function fetchEdemamData(filters = []) {
 
     try {
       const response = await axios.request(options);
-    
-      console.log(response.data);
+      return response.data;
     } 
     catch (error) 
     {

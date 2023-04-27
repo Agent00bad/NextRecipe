798b7d5f4564aa446a41ad89d48aa0be86aa825e
Daniel Aldén .NET <Daniel Aldn .NET@DESKTOP-0QL2OQM>
2023-04-27 12:18:04 +0200

    Fixed basic functionallity

---
+++ react/src/JS/fetchApiData.js
@@ -0,0 +1,34 @@
+export async function fetchEdemamData(filters = []) {
+    let filterQuery;
+
+    if(filters > 0)
+    {
+        filters.map(filter => {
+            filterQuery += ` ${filter}`;
+        })
+    }
+
+    const axios = require("axios");
+    const options = {
+      method: "GET",
+      url: "https://edamam-recipe-search.p.rapidapi.com/search",
+      params: {
+        q: filters > 0 ? filterQuery : "recipe",
+      },
+      headers: {
+        "content-type": "application/octet-stream",
+        "X-RapidAPI-Key": "df8a80b1a6msha38ff4bdfa0adc0p18c309jsn16a0454d60dc",
+        "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+    
+      console.log(response.data);
+    } 
+    catch (error) 
+    {
+        return error;
+    }
+}
--- react/src/root.jsx
+++ react/src/root.jsx
@@ -1,15 +1,30 @@
 import { Outlet } from "react-router-dom"
 import Header from "./component/header/header"
 import Footer from "./component/footer/footer"
+import { useEffect, useState } from "react"
+import { fetchEdemamData } from "./JS/fetchApiData"
 export default function Root()
 {
+    const [data, setData] = useState(null);
+    const [key, setKey] = useState("9ed58f54f91135c58c96fd426518175a");
+    const [url,setUrl] = useState("https://api.edamam.com/api/recipes/v2?type=public&app_id=67dcdfc6&app_key=9ed58f54f91135c58c96fd426518175a&mealType=Breakfast");
+    useEffect(() => {
+        const getData = async () =>{
+            const result = await fetchEdemamData();
+            setData(result);
+        }
+        getData();
+        console.log(data);
+    },[])
     return(
         <div className="root">
             <header>
                 <Header/>
             </header>
             <main>
-                <Outlet/>
+                {data && 
+                <p>{data.length}</p>
+                }
             </main>
             <footer>
                 <Footer/>

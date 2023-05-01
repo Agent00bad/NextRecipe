80dff0383aa08ebb471861e2f5ad87f76f58c123
Daniel Aldén .NET <Daniel Aldn .NET@DESKTOP-0QL2OQM>
2023-05-01 14:44:59 +0200

    removed code from root that is not supposed to be there

--- react/src/root.jsx
+++ react/src/root.jsx
@@ -1,43 +1,21 @@
-import { Outlet } from "react-router-dom"
-import Header from "./component/header/header"
-import Footer from "./component/footer/footer"
-import { useEffect, useState } from "react"
-import { fetchApiEdemam } from "./JS/fetchApiEdamam"
-import { fetchApiSpoonacular } from "./JS/fetchApiSpoonacular"
-export default function Root()
-{
-    const filterInput = ["Breakfast", "Lunch"]
-    const [data, setData] = useState(null);
-    const [wines, setWines] = useState(null);
-    useEffect(() => {
-        const getData = async () =>{
-            const result = await fetchApiEdemam(filterInput);
-            setData(result);
-            const wineResult = await fetchApiSpoonacular("chicken");
-            setWines(wineResult);
-        }
-        getData();
-        console.log(wines);
-    },[])
-    return(
-        <div className="root">
-            <header>
-                <Header/>
-            </header>
-            <main>
-                { wines && 
-                <div>
-                    <Outlet/>
-                    <h2>{wines.title}</h2>
-                    <p>{wines.pairingText}</p>
-                    <img src={wines.imageUrl} alt="" />
-                </div>
-                }
-            </main>
-            <footer>
-                <Footer/>
-            </footer>
-        </div>
-    )
-
+import { Outlet } from "react-router-dom";
+import Header from "./component/header/header";
+import Footer from "./component/footer/footer";
+import { useEffect, useState } from "react";
+import { fetchApiEdemam } from "./JS/fetchApiEdamam";
+import { fetchApiSpoonacular } from "./JS/fetchApiSpoonacular";
+export default function Root() {
+  return (
+    <div className="root">
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }

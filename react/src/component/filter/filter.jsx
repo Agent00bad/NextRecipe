b01b7873e686927ca960151df2de4cabca0b47a6
Daniel Aldén .NET <Daniel Aldn .NET@DESKTOP-0QL2OQM>
2023-05-16 14:55:48 +0200

    Fixed so you can click on the entire filter div to toggle it instead of only the arrows

--- react/src/component/filter/filter.jsx
+++ react/src/component/filter/filter.jsx
@@ -7,58 +7,76 @@ import arrowDown from "../../Images/arrow-down.png";
 import CheckMarkSquare from "../../images/pink-square.png";
 import { useOutletContext } from "react-router-dom";
 import { Link, useLocation } from "react-router-dom";
-import {Routes, Route, useNavigate} from 'react-router-dom';
-
-
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 export default function Filter() {
-
   const [activeFilters, setActiveFilters] = useOutletContext();
   const mealType = ["Breakfast", "Lunch", "Dinner", "Snacks"];
-  const allergies = ["Vegeterian", "Vegan", "Gluten free", "Lactose free", "Milk free", "Nut free"];
+  const allergies = [
+    "Vegeterian",
+    "Vegan",
+    "Gluten free",
+    "Lactose free",
+    "Milk free",
+    "Nut free",
+  ];
   const proteins = ["Steak", "Pork", "Seafood", "Chicken"];
   const apply = ["Apply"];
   const clear = ["Clear"];
-  const[toggleMeals, setToggleMeals] = useState(true);
-  const[toggleAllergies, setToggleAllergies] = useState(true);
-  const[toggleProteins, setToggleProteins] = useState(true);
-
+  const [toggleMeals, setToggleMeals] = useState(true);
+  const [toggleAllergies, setToggleAllergies] = useState(true);
+  const [toggleProteins, setToggleProteins] = useState(true);
 
   const navigate = useNavigate();
   const navigateToIndex = () => {
-    navigate('/')
-  }
+    navigate("/");
+  };
 
   function setFilter(filter) {
     if (filter != activeFilters.find((f) => f === filter)) {
-      setActiveFilters(oldFilters => [...oldFilters, filter]);
-    } 
-    else {
-      setActiveFilters(filters => filters.filter(oldFilter => oldFilter != filter))
+      setActiveFilters((oldFilters) => [...oldFilters, filter]);
+    } else {
+      setActiveFilters((filters) =>
+        filters.filter((oldFilter) => oldFilter != filter)
+      );
     }
   }
 
-  const removeFilterChoices = () =>{
-    setActiveFilters(activeFilters.filter(filter => null))
-  }
+  const removeFilterChoices = () => {
+    setActiveFilters(activeFilters.filter((filter) => null));
+  };
 
   return (
     <>
-        <div className="filterandbutton">
-            <main>
-              <SectionMobileMeals header="Meal Options" id="1"/>
-              <Pills types={mealType} activeFilter={activeFilters} setFilter={setFilter} toggle={toggleMeals} />
-              <SectionMobileAllergies header="Meal Allergies/Diet" id="2"/>
-              <Pills types={allergies} activeFilter={activeFilters} setFilter={setFilter} toggle={toggleAllergies}/>
-              <SectionMobileProteins header="Type of food" id="3"/>
-              <Pills types={proteins} activeFilter={activeFilters} setFilter={setFilter} toggle={toggleProteins} />
-              <div className="flex-container-horizontal apply-clear">
-                <ApplyClearPills  types={apply} id="apply-button" />
-                <ApplyClearPills types={clear} id="clear-button" />
-              </div>
+      <div className="filterandbutton">
+        <main>
+          <SectionMobileMeals header="Meal Options" id="1" />
+          <Pills
+            types={mealType}
+            activeFilter={activeFilters}
+            setFilter={setFilter}
+            toggle={toggleMeals}
+          />
+          <SectionMobileAllergies header="Meal Allergies/Diet" id="2" />
+          <Pills
+            types={allergies}
+            activeFilter={activeFilters}
+            setFilter={setFilter}
+            toggle={toggleAllergies}
+          />
+          <SectionMobileProteins header="Type of food" id="3" />
+          <Pills
+            types={proteins}
+            activeFilter={activeFilters}
+            setFilter={setFilter}
+            toggle={toggleProteins}
+          />
+          <div className="flex-container-horizontal apply-clear">
+            <ApplyClearPills types={apply} id="apply-button" />
+            <ApplyClearPills types={clear} id="clear-button" />
+          </div>
         </main>
-     </div>
-
+      </div>
     </>
   );
 
@@ -74,86 +92,83 @@ export default function Filter() {
     );
   }
 
-  function SectionMobileMeals({header}) {
-    const handleClick = () =>{
-      setToggleMeals(!toggleMeals)
-    }
+  function SectionMobileMeals({ header }) {
+    const handleClick = () => {
+      setToggleMeals(!toggleMeals);
+    };
     return (
-      <div className="filterandbutton">
+      <div className="filterandbutton" onClick={handleClick}>
         <div className="flex-container-horizontal">
           <h2>{header}</h2>
           <button
-            onClick={handleClick}
             className={toggleMeals ? "arrow arrowdown" : "arrow arrowup"}
             alt="Arrow down"
           ></button>
         </div>
-        <hr/>
-      </div>
-    );
-  }
-  
-  function SectionMobileAllergies({header}) {
-    const handleClick = () =>{
-      setToggleAllergies(!toggleAllergies)
-    }
-    return (
-      <div className="filterandbutton">
-        {!toggleMeals ? <hr/> : ""}
-        <div className="flex-container-horizontal">
-          <h2>{header}</h2>
-          <button
-            onClick={handleClick}
-            className={toggleAllergies ? "arrow arrowdown" : "arrow arrowup"}
-            alt="Arrow down"
-          ></button>
-        </div>
-        <hr/>
-      </div>
-    );
-  }
-  
-  function SectionMobileProteins({header}) {
-    const handleClick = () =>{
-      setToggleProteins(!toggleProteins)
-    }
-    return (
-      <div className="filterandbutton">
-        {!toggleAllergies ? <hr/> : ""}
-        <div className="flex-container-horizontal">
-          <h2>{header}</h2>
-          <button
-            onClick={handleClick}
-            className={toggleProteins ? "arrow arrowdown" : "arrow arrowup"}
-            alt="Arrow down"
-          ></button>
-        </div>
-        <hr/>
+        <hr />
       </div>
     );
   }
 
- 
+  function SectionMobileAllergies({ header }) {
+    const handleClick = () => {
+      setToggleAllergies(!toggleAllergies);
+    };
+    return (
+      <div className="filterandbutton" onClick={handleClick}>
+        {!toggleMeals ? <hr /> : ""}
+        <div className="flex-container-horizontal">
+          <h2>{header}</h2>
+          <button
+            className={toggleAllergies ? "arrow arrowdown" : "arrow arrowup"}
+            alt="Arrow down"
+          ></button>
+        </div>
+        <hr />
+      </div>
+    );
+  }
+
+  function SectionMobileProteins({ header }) {
+    const handleClick = () => {
+      setToggleProteins(!toggleProteins);
+    };
+    return (
+      <div className="filterandbutton" onClick={handleClick}>
+        {!toggleAllergies ? <hr /> : ""}
+        <div className="flex-container-horizontal">
+          <h2>{header}</h2>
+          <button
+            className={toggleProteins ? "arrow arrowdown" : "arrow arrowup"}
+            alt="Arrow down"
+          ></button>
+        </div>
+        <hr />
+      </div>
+    );
+  }
 
   // -------    BOTH    -------
 
   function ApplyClearPills({ types, id }) {
     return (
       <>
-      <div className="filterandbutton">
-        {types.map((data) => (
-          <button
-            className="filter-button"
-            id={id}
-            onClick={id == "apply-button" ? navigateToIndex : removeFilterChoices} >
-            {data}
-          </button>
-        ))}
+        <div className="filterandbutton">
+          {types.map((data) => (
+            <button
+              className="filter-button"
+              id={id}
+              onClick={
+                id == "apply-button" ? navigateToIndex : removeFilterChoices
+              }
+            >
+              {data}
+            </button>
+          ))}
         </div>
       </>
     );
   }
-
 
   // -------    DESKTOP    -------
 
@@ -185,8 +200,8 @@ export default function Filter() {
       <>
              {" "}
         <div>
-                  <h2>{header}</h2>        {/* <button id="arrowButton"> */}
-                   {" "}
+                  <h2>{header}</h2>        {/* <button id="arrowButton"> */}   
+               {" "}
           <img
             className="arrow"
             src={arrowDown}

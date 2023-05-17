674b3b0373760ec6d10789e8db982cb14c15bcf2
Imir <imir.norlin96@hotmail.com>
2023-05-17 10:06:21 +0200

    Updated recipe site - drink now won't take up extra space when not loaded in - changed pixel format to fit with current design - fixed meal image to show as a square and be more in line with the other blocks

--- react/src/component/recipe/recipeMain.jsx
+++ react/src/component/recipe/recipeMain.jsx
@@ -115,7 +115,18 @@ export default function Recipe() {
           {/* Flex container - Center */}
         </div>
         {/* Flex container Right */}
-        <div className="dynamic-flex-column">
+        {wineIsNull(wineData)}
+        {/* Flex container Right */}
+      </div>
+    </div>
+  );
+}
+
+
+function wineIsNull(wineData){
+if(wineData != null){
+  return(
+<div    className="dynamic-flex-column">
           {/* Recommended Drink */}
           {wineData && (
             <div className="loaded-drink fixedMargin">
@@ -128,24 +139,9 @@ export default function Recipe() {
           )}
           {/* Flex container Right */}
 
-          {/* <div className="drinkGroup fixedMargin">
-            <div className="placeholder ">
-              <p>
-                “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus
-                rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae
-                volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui
-                fringilla consectetur id nec massa. Aliquam erat volutpat. Sed
-                ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed
-                lacinia lectus. Duis sit amet sodales felis. Duis nunc eros,
-                mattis at dui ac, convallis semper risus. In adipiscing ultrices
-                tellus, in suscipit massa vehicula eu.”
-              </p>
-            </div>
-          </div> */}
+          
         </div>
-        {/* Flex container Right */}
-      </div>
-    </div>
-  );
+
+  )
+}
 }
